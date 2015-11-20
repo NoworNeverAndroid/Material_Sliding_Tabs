d0d1f8b05b58b1278d804eb87c20e7f270448d26
NowOrNeverAndroid <noworneverandroid@outlook.es>
2015-11-20 12:23:13 +0100

    Initial commit

---
+++ app/src/main/java/com/example/nowor_000/androidslidingtabs/Tab1.java
@@ -0,0 +1,34 @@
+package com.example.nowor_000.androidslidingtabs;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class Tab1 extends Fragment {
+
+
+    Button button;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.tab_1,container,false);
+
+        View v2 =v.findViewById(R.id.textView);
+
+
+        return v;
+    }
+
+
+}
